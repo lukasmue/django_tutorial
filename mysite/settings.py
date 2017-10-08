11d3688bfa435e516ceab166726c1e2d64a8d131
Lukas Müller <lukas.mueller0289@icloud.com>
2017-10-08 12:46:13 +0200

    modified:   mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'mydatabase',
-#        'USER': 'mydatabaseuser',
-#        'PASSWORD': 'mypassword',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
 DATABASES = {'default': dj_database_url.config()}
